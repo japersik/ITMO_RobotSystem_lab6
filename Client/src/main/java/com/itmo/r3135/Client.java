52e6a6544a463b41f9744bb667802f87d5488927
marukhlenkodaniil <daniil.marukh@gmail.com>
2020-03-23 19:25:36 +0300

    Test client-server

--- Client/src/main/java/com/itmo/r3135/Client.java
+++ Client/src/main/java/com/itmo/r3135/Client.java
@@ -1,7 +1,37 @@
 package com.itmo.r3135;
 
-public class Client {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
 
+public class Client {
+    public static int port = 2622;
+    static byte[] b = {1, 2, 3, 4, 5, 62, 3, 4, 3, 5};
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Запуск клиента....");
+        SocketAddress addres = new InetSocketAddress("localhost", port);
+        System.out.println("Запуск прошёл успешно, Потр: " + port + ". Адрес: " + addres);
+        System.out.println("Открытае канала");
+        DatagramChannel channel = DatagramChannel.open();
+        //channel.socket().bind(addres);
+       // channel.connect(addres);
+        System.out.println("Канал открыт");
+        System.out.println(addres);
+        ByteBuffer buffer = ByteBuffer.wrap(b);
+       // buffer.flip();
+        System.out.println("Отправление");
+        channel.send(buffer,addres);
+        System.out.println("Отправлено");
+        buffer.clear();
+        channel.receive(buffer);
+        buffer.flip();
+        channel.close();
+        System.out.println("Канал закрыт. ");
+        System.out.println("Принято:");
+        for (byte j : b) {
+            System.out.println(j);
+        }
     }
 }
--- Server/src/main/java/com/itmo/r3135/Server.java
+++ Server/src/main/java/com/itmo/r3135/Server.java
@@ -1,7 +1,41 @@
 package com.itmo.r3135;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
 
+/**
+ * @author daniil
+ * @author vladislav
+ */
+public class Server {
+    private static int port = 2622;
+    static byte[] b = new byte[10];
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Инициализация сервера:");
+        DatagramSocket socket = new DatagramSocket(port);
+        System.out.println("Запуск прошёл успешно, Потр: " + port);
+        DatagramPacket input = new DatagramPacket(b, b.length);
+        while (true) {
+            System.out.println("Ожидание");
+            socket.receive(input);
+            System.out.println(input);
+            System.out.println("Принято:");
+            for (byte j : b) {
+                System.out.println(j);
+            }
+            System.out.println("Обработка (умножение на 2)");
+            for (int ki = 0; ki < 10; ki++) {
+                b[ki] *= 2;
+            }
+            InetAddress addres = input.getAddress();
+            int outPort = input.getPort();
+            System.out.println("Отправка");
+            DatagramPacket output = new DatagramPacket(b, b.length, addres, outPort);
+            socket.send(output);
+            System.out.println("Опправлено.");
+        }
     }
 }
