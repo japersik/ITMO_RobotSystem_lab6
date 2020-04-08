c27b4cf2411f00d79d4402030cc07aff94bb20c5
marukhlenkodaniil <daniil.marukh@gmail.com>
2020-04-08 18:18:04 +0300

    java 1.8 fix

--- Server/src/main/java/com/itmo/r3135/ServerWorker.java
+++ Server/src/main/java/com/itmo/r3135/ServerWorker.java
@@ -2,7 +2,11 @@ package com.itmo.r3135;
 
 import com.google.gson.Gson;
 import com.itmo.r3135.Commands.*;
-import com.itmo.r3135.System.*;
+import com.itmo.r3135.System.Command;
+import com.itmo.r3135.System.CommandList;
+import com.itmo.r3135.System.ServerMessage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,8 +15,8 @@ import java.net.SocketException;
 import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
-
 public class ServerWorker implements Mediator {
+    static final Logger logger = LogManager.getLogger("ServerWorker");
     private static final Semaphore SEMAPHORE = new Semaphore(1, true);
     private int port;
     private DatagramSocket socket;
@@ -63,38 +67,48 @@ public class ServerWorker implements Mediator {
 
     public ServerWorker(int port, String fileName) {
         this.port = port;
+        logger.info("Server port: " + port);
         if (fileName == null) {
-            System.out.println("Путь к файлу json не обнаружен.");
+//            System.out.println("Путь к файлу json не обнаружен.");
+            logger.warn("File path " + fileName + " is wrong!!!");
             System.exit(1);
         }
         File jsonPath = new File(fileName);
         if (jsonPath.exists()) {
             collection.setJsonFile(jsonPath);
-            System.out.println("Адрес " + this.collection.getJsonFile().toString() + " успешно обнаружен");
+//            System.out.println("Адрес " + this.collection.getJsonFile().toString() + " успешно обнаружен.");
+            logger.info("Path " + fileName + " discovered.");
         } else {
-            System.out.println("Указанного пути не существует.");
+//            System.out.println("Указанного пути не существует.");
+            logger.warn("Path " + fileName + " does not exist.");
             System.exit(1);
         }
         if (!jsonPath.isFile()) {
-            System.out.println("Путь " + jsonPath.toString() + " не содержит имени файла");
+//            System.out.println("Путь " + jsonPath.toString() + " не содержит имени файла");
+            logger.warn("Path " + fileName + " does not contain a file name.");
             System.exit(1);
         } else {
-            System.out.println("Файл " + jsonPath.toString() + " успещно обнаружен.");
+//            System.out.println("Файл " + jsonPath.toString() + " успещно обнаружен.");
+            logger.info("File " + fileName + " discovered.");
         }
         if (!(fileName.lastIndexOf(".json") == fileName.length() - 5)) {
-            System.out.println("Заданный файл не в формате .json");
+//            System.out.println("Заданный файл не в формате .json");
+            logger.warn("Non .json file format.");
             System.exit(1);
         }
+        logger.info("File is " + jsonPath.getAbsolutePath());
     }
 
     public void startWork() throws SocketException {
         System.out.println("Инициализация сервера.");
+        logger.info("Server initialization.");
         socket = new DatagramSocket(port);
         sender = new Sender(socket);
         reader = new Reader(socket);
         System.out.println("Загрузка коллекции.");
         loadCollectionCommand.activate(new Command(CommandList.LOAD));
         System.out.println("Запуск прошёл успешно, Потр: " + port);
+        logger.info("Server started.");
         Thread keyBoard = new Thread(() -> keyBoardWork());
         Thread datagramm = new Thread(() -> datagrammWork());
         keyBoard.setDaemon(false);
@@ -113,10 +127,12 @@ public class ServerWorker implements Mediator {
                     SEMAPHORE.acquire();
                     switch (inputString) {
                         case "exit":
+                            logger.info("Command 'exit' from console.");
                             processing(new Command(CommandList.SAVE));
                             processing(new Command(CommandList.EXIT));
                             break;
                         case "save":
+                            logger.info("Command 'save' from console.");
                             processing(new Command(CommandList.SAVE));
                             break;
                         default:
@@ -136,67 +152,69 @@ public class ServerWorker implements Mediator {
                 Command command = reader.nextCommand();
                 SEMAPHORE.acquire();
                 System.out.println("Принято:");
+                logger.info("Command " + command.getCommand() + " from " + reader.getInput().getSocketAddress() + ".");
                 System.out.println(command.getCommand());
                 System.out.println(command.getString());
-                sender.send(processing(command),reader.getInput());
-//                Thread.sleep(3000); Для отладки
+                ServerMessage message = processing(command);
+                logger.info("Command complete.");
+                logger.info("Sending server message.");
+                sender.send(message, reader.getInput());
+//                Thread.sleep(3000);// Для отладки
                 SEMAPHORE.release();
             } catch (IOException | InterruptedException e) {
                 System.out.println("Ошибка сериализации");
+                logger.error("Error in receive-send command!!!");
             }
         }
     }
 
     @Override
     public ServerMessage processing(Command command) {
-        try {
-            try {
-                switch (command.getCommand()) {
-                    case CHECK:
-                        System.out.println("Попытка соединиться");
-                        return new ServerMessage("Good connect. Hallo from server!");
-                    case HELP:
-                        return helpCommand.activate(command);
-                    case INFO:
-                        return infoCommand.activate(command);
-                    case SHOW:
-                        return showCommand.activate(command);
-                    case ADD:
-                        return addCommand.activate(command);
-                    case UPDATE:
-                        return updeteIdCommand.activate(command);
-                    case REMOVE_BY_ID:
-                        return removeByIdCommand.activate(command);
-                    case CLEAR:
-                        return clearCommand.activate(command);
-                    case EXECUTE_SCRIPT:
-                        return executeScriptCommand.activate(command);
-                    case ADD_IF_MIN:
-                        return addIfMinCommand.activate(command);
-                    case REMOVE_GREATER:
-                        return removeGreaterCommand.activate(command);
-                    case REMOVE_LOWER:
-                        return removeLowerCommand.activate(command);
-                    case GROUP_COUNTING_BY_COORDINATES:
-                        return groupCountingByCoordinatesCommand.activate(command);
-                    case FILTER_CONTAINS_NAME:
-                        return filterContainsNameCommand.activate(command);
-                    case PRINT_FIELD_DESCENDING_PRICE:
-                        return printFieldDescendingPriceCommand.activate(command);
-                    case SAVE:
-                        return saveCommand.activate(command);
-                    case EXIT:
-                        return exitCommand.activate(command);
-                    default:
-                        System.out.println("Неопознанная команда.");
-                }
-            } catch (NumberFormatException ex) {
-                System.out.println("Где-то проблема с форматом записи числа.Команда не выполнена");
-            }
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            System.out.println("Отсутствует аргумент.");
-        }
-        return new ServerMessage("Битая команда");
 
+        try {
+            switch (command.getCommand()) {
+                case CHECK:
+                    return new ServerMessage("Good connect. Hallo from server!");
+                case HELP:
+                    return helpCommand.activate(command);
+                case INFO:
+                    return infoCommand.activate(command);
+                case SHOW:
+                    return showCommand.activate(command);
+                case ADD:
+                    return addCommand.activate(command);
+                case UPDATE:
+                    return updeteIdCommand.activate(command);
+                case REMOVE_BY_ID:
+                    return removeByIdCommand.activate(command);
+                case CLEAR:
+                    return clearCommand.activate(command);
+                case EXECUTE_SCRIPT:
+                    return executeScriptCommand.activate(command);
+                case ADD_IF_MIN:
+                    return addIfMinCommand.activate(command);
+                case REMOVE_GREATER:
+                    return removeGreaterCommand.activate(command);
+                case REMOVE_LOWER:
+                    return removeLowerCommand.activate(command);
+                case GROUP_COUNTING_BY_COORDINATES:
+                    return groupCountingByCoordinatesCommand.activate(command);
+                case FILTER_CONTAINS_NAME:
+                    return filterContainsNameCommand.activate(command);
+                case PRINT_FIELD_DESCENDING_PRICE:
+                    return printFieldDescendingPriceCommand.activate(command);
+                case SAVE:
+                    return saveCommand.activate(command);
+                case EXIT:
+                    return exitCommand.activate(command);
+                default:
+                    System.out.println("Неопознанная команда.");
+            }
+        } catch (NumberFormatException ex) {
+            System.out.println("Где-то проблема с форматом записи числа.Команда не выполнена");
+            logger.error("Bad number in command!!!");
+        }
+        logger.warn("Bad command!");
+        return new ServerMessage("Битая команда");
     }
 }
